9fc81a3d2a7a6dc0c9b04dbf0d85d88a16f57ddf
Chris Philbin <chrisp@runway.com.au>
2022-02-16 06:43:48 -0500

    Adding basic authentication functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { PostsComponent } from './components/posts/posts.component';
 
 const routes: Routes = [
   { path: '', component: PostsComponent },
+  { path: 'signin', component: AuthComponent },
   { path: 'category/:id', component: CategoriesComponent },
 ];
 
